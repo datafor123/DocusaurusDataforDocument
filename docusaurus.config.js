b6ca1afda6eb50ac48f0994ccb487f2f64022eca
meishenlieshou <meishenlieshou@users.noreply.github.com>
2022-08-22 15:39:18 +0800

    编辑页跳转地址调试

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+						"https://github.com/datafor123/DocusaurusDataforDocument/edit/main"
 				},
 				pages: {
 					path: "src/pages",
